719fc230015edc5702c4a40fd8f9db53d322bd96
denizz55 <lekin.den@mail.ru>
2023-12-19 02:29:14 +0300

    форма изображения

--- Staff/myapp/forms.py
+++ Staff/myapp/forms.py
@@ -1,10 +1,11 @@
 from .models import Task
 from django.forms import ModelForm, TextInput
-
+from django.forms import ModelForm, TextInput, FileField, ClearableFileInput
+from django.forms import FileInput
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ["name", "place", "price", "square"]
+        fields = ["name", "place", "price", "square", "img"]
         widgets ={
             "name": TextInput(attrs={
                 'class': 'form-control w-75',
@@ -21,5 +22,9 @@ class TaskForm(ModelForm):
             "square": TextInput(attrs={
                 'class': 'form-control w-75',
                 'placeholder': 'Адрес заказа'
+            }),
+            "img": FileInput(attrs={
+                'class': 'p_files w-75',
+                'placeholder': 'Файлы'
             }),
         }
--- Staff/myapp/models.py
+++ Staff/myapp/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField('Название', max_length=100)
-    price = models.IntegerField('Цена', max_length=100)
-    place = models.IntegerField('Количество', default=1)
+    price = models.IntegerField('Цена')
+    place = models.IntegerField('Количество')
+    img = models.ImageField(upload_to="img", default=None, blank=True, null=True, verbose_name="Фото")
     square = models.CharField('Адрес заказа', max_length=100)
+
+
+    @property
+    def photo_url(self):
+        if self.img and hasattr(self.img, 'url'):
+            return self.img.url
 
     def __str__(self):
         return self.name
